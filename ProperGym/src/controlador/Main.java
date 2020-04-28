f600b472585c694fd430bce0addcd76062406e64
DanyCB <danicruba67@gmail.com>
2020-04-28 13:06:30 +0200

    New Update

--- ProperGym/src/controlador/Main.java
+++ ProperGym/src/controlador/Main.java
@@ -1,11 +1,15 @@
 package controlador;
 
+import java.awt.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+	public static String pathA = "loginA.txt";
+	public static String pathC = "loginC.txt";
 	
 	public static int menuPrincipal(Scanner sc) {
-		System.out.println("Bienfkwfkwebfwe");
+		System.out.println("<-------------------BIENVENIDO A PROPERGYM--------------------->");
 		System.out.println("Digite el numero de su elecciÛn:");
 		System.out.println("1. Administrador");
 		System.out.println("2. Cliente");
@@ -17,72 +21,120 @@ public class Main {
 		switch (opcion) {
 		case 1:
 			logA=loginAdministrador(sc);
-			opcion = sc.nextInt();
-			if(opcion == 2) {
-				menuPrincipal(sc);
-			}
 			break;
 		case 2:
 			logC=loginCliente(sc);
-			opcion = sc.nextInt();
-			if(opcion == 2) {
-				opcion = menuPrincipal(sc);
-			}
 			break;
 		case 3:
-			visualizarActiv();
-			System.out.println("Digite el numero de su elecciÛn:");
-			System.out.println("1. Registrarse/MembresÌa");
-			System.out.println("2. Atr·s");
-			opcion = sc.nextInt();
-			if(opcion == 2) {
-				opcion = menuPrincipal(sc);
-			}else {
-				registro();
-			}
+			visualizarActivE(sc);
 			break;
 		}
 		return opcion;
 		
 	}
 	
-	public static void registro() {
-		System.out.println("Ingrese datos");
-		System.out.println("Ingrese datos");
-		System.out.println("Ingrese datos");
-		System.out.println("Ingrese datos");
-		System.out.println("Ingrese datos");
-		System.out.println("Ingrese datos");
+	public void limpiar(int lineas)
+	{
+	 for (int i=0; i < lineas; i++)
+	 {
+	  System.out.println();
+	 }
+	}
+	
+	public static void registro(Scanner sc) {
+		Scanner reg = new Scanner(System.in);
+		String usuario;
+		String contrasena;
+		String Nombre;
+		String Apellido;
+		String fnac;
+		String correo;
+		System.out.println("Ingrese usuario:");
+		usuario = reg.nextLine();
+		System.out.println("Ingrese contraseÒa:");
+		contrasena = reg.nextLine();
+		System.out.println("Ingrese Primer Nombre:");
+		Nombre = reg.nextLine();
+		System.out.println("Ingrese Primer Apellido:");
+		Apellido = reg.nextLine();
+		System.out.println("Ingrese Fecha de Nacimiento:");
+		fnac = reg.nextLine();
+		System.out.println("Ingrese Correo ElectrÛnico:");
+		correo = reg.nextLine();
+		String [] cliente = {usuario, contrasena, Nombre, Apellido, fnac, correo};
+		
 	}
 	
 	public static boolean loginAdministrador(Scanner sc) {
-		boolean logeado = true;
-		String usuarioA = "";
-		String contrasenaA = "";
-		System.out.println("Login Administrador");
-		System.out.println("Usuario");
-		usuarioA = sc.nextLine();
-		System.out.println("ContraseÒa");
-		contrasenaA = sc.nextLine();
-		System.out.println("2. Atr·s");
 		
+		boolean logeado = true;
+		String usuarioA;
+		String contrasenaA;
+		int opcion = 0;
+		Scanner admi = new Scanner(System.in);
+		
+		System.out.println("<---------------------LOGIN ADMINISTRADOR------------------->");
+		System.out.println("Usuario");
+		usuarioA = admi.nextLine();
+		System.out.println("ContraseÒa");
+		contrasenaA = admi.nextLine();
+		LectorCSVA lec = new LectorCSVA();
+		
+		logeado = lec.readFileA(pathA, usuarioA, contrasenaA);
+		
+		
+		if(logeado == true) {
+			visualizarActiv(sc);
+		}else {
+			System.out.println("Login Incorrecto");
+			System.out.println("1. Volver a intentar");
+			System.out.println("2. Atr·s");
+			opcion = admi.nextInt();
+			if(opcion == 1) {
+				logeado=loginAdministrador(sc);
+			}else if(opcion == 2) {
+				menuPrincipal(sc);
+			}
+		}
 		return logeado;
 	}
 	
 	public static boolean loginCliente(Scanner sc) {
 		boolean logeado = true;
-		String usuarioC = "";
-		String contrasenaC = "";
-		System.out.println("Login Cliente");
-		System.out.println("Usuario");
-		usuarioC = sc.nextLine();
-		System.out.println("Contraseña");
-		contrasenaC = sc.nextLine();
-		System.out.println("2. Atrás");
+		String usuarioC;
+		String contraseÒaC;
+		int opcion = 0;
+		Scanner cliente = new Scanner(System.in);
+		
+		System.out.println("<---------------------LOGIN CLIENTE------------------------->");
+		System.out.println("Ingrese su usuario:");
+		usuarioC = cliente.nextLine();
+		System.out.println("Ingrese su contraseÒa:");
+		contraseÒaC = cliente.nextLine();
+		LectorCSVA lec = new LectorCSVA();
+		
+		logeado = lec.readFileA(pathC, usuarioC, contraseÒaC);
+		
+		if(logeado == true) {
+			visualizarActiv(sc);
+		}else {
+			System.out.println("Login Incorrecto");
+			System.out.println("1. Volver a intentar");
+			System.out.println("2. Atr·s");
+			opcion = cliente.nextInt();
+			if(opcion == 1) {
+				logeado=loginCliente(sc);
+			}else if(opcion == 2) {
+				menuPrincipal(sc);
+			}
+		}
 		return logeado;
 	}
 	
-	public static void visualizarActiv() {
+	public static void visualizarActivE(Scanner sc) {
+		Scanner vis = new Scanner(System.in);
+		int opcion = 0;
+		System.out.println("<---------------------VISUALIZAR ACTIVIDADES------------------------->");
 		System.out.println("Actividades del Gimnasio");
 		System.out.println("Actividades del Gimnasio");
 		System.out.println("Actividades del Gimnasio");
@@ -90,13 +142,40 @@ public class Main {
 		System.out.println("Actividades del Gimnasio");
 		System.out.println("Actividades del Gimnasio");
 		System.out.println("Actividades del Gimnasio");
+		System.out.println("Digite el numero de su elecciÛn:");
+		System.out.println("1. Registrarse/MembresÌa");
+		System.out.println("2. Atr·s");
+		opcion = vis.nextInt();
+		if(opcion == 2) {
+			opcion = menuPrincipal(sc);
+		}else {
+			registro(sc);
+		}
+	}
+	
+	public static void visualizarActiv(Scanner sc) {
+		Scanner vis = new Scanner(System.in);
+		int opcion = 0;
+		System.out.println("<---------------------VISUALIZAR ACTIVIDADES------------------------->");
+		System.out.println("Actividades del Gimnasio");
+		System.out.println("Actividades del Gimnasio");
+		System.out.println("Actividades del Gimnasio");
+		System.out.println("Actividades del Gimnasio");
+		System.out.println("Actividades del Gimnasio");
+		System.out.println("Actividades del Gimnasio");
+		System.out.println("Actividades del Gimnasio");
+		System.out.println("1. Atr·s");
+		opcion = vis.nextInt();
+		if(opcion == 1) {
+			opcion = menuPrincipal(sc);
+		}
 	}
 	
 	public static void main(String[] args) {
 		String usuarioA = "";
-		String contrasenaA = "";
+		String contraseÒaA = "";
 		String usuarioC = "";
-		String contrasenaC = "";
+		String contraseÒaC = "";
 		boolean logA = false;
 		boolean logC = false;
 		Scanner sc = new Scanner(System.in);
@@ -106,3 +185,6 @@ public class Main {
 	}
 
 }
+
+
+
